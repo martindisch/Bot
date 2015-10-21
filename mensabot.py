972e614e99af96d775fff2ab87d98e358720814c
Martin Disch <martindisch@gmail.com>
2015-10-21 13:58:09 +0200

    Started mensabot

---
+++ mensabot.py
@@ -0,0 +1,83 @@
+import requests
+import json
+import os
+from time import sleep
+from pprint import pprint
+
+# Read token from untracked credentials file.
+# We wouldn't want this on Github now, would we?
+f = open("creds.txt")
+lines = f.readlines()
+f.close
+token = lines[0].strip()
+print "Token loaded"
+
+# If it exists, read the previous last update id from file
+last_update = 0
+if os.path.isfile("offset"):
+    f = open("offset", 'r')
+    last_update = int(f.read())
+    f.close
+    print "Offset loaded"
+else:
+    print "No offset - starting new"
+
+url = 'https://api.telegram.org/bot%s/' % token
+
+pollCreator = "null"
+participants_id = []
+participants_name = []
+
+while True:
+    gotUpdate = False
+    get_updates = json.loads(requests.get(url + 'getUpdates').content)
+    for update in get_updates['result']:
+        if last_update < update['update_id']:
+            gotUpdate = True
+            msg = update['message']['text']
+            if msg == "/newpoll":
+                if pollCreator == "null":
+                    pollCreator = update['message']['from']['id']
+                    reply = "Who's also having lunch today?"
+                    out = update['message']['from']['first_name'] + " started a poll"
+                else:
+                    reply = "There's already a poll running"
+                    out = update['message']['from']['first_name'] + " tried to start another poll"
+            elif msg == "/me":
+                if any(update['message']['from']['id'] in p for p in participants_id):
+                    out = update['message']['from']['first_name'] + " tried to add themselves again"
+                else:
+                    participants_id += update['message']['from']['id']
+                    participants_name += update['message']['from']['first_name']
+                    out = update['message']['from']['first_name'] + " joins for lunch"
+                    reply = "Got it"
+            elif msg == "/result":
+                if pollCreator != "null":
+                    if pollCreator == update['message']['from']['id']:
+                        if len(participants_name) > 2:
+                            test = 0
+                        elif len(participants_name) == 2:
+                            reply = participants_name[0] + " and " + participants_name[1] + " are joining you today.\n\nBe sure to save two more seats."
+                        elif len(participants_name) == 1:
+                            reply = participants_name[0] + "is joining you today.\n\nBe sure to save one more seat."
+                        else:
+                            reply = "Looks like nobody is coming today"
+                        pollCreator = "null"
+                        participants_id = []
+                        participants_name = []
+                    else:
+                        out = update['message']['from']['first_name'] + " tried to finish a poll they didn't start"
+                        reply = "Only the creator can finish a poll"
+                else:
+                    out = update['message']['from']['first_name'] + " tried to finish a poll - there is none running"
+                    reply = "There is no poll running"
+            print out
+            last_update = update['update_id']
+            requests.get(url + 'sendMessage', params=dict(chat_id=update['message']['chat']['id'], text=reply))
+            print "Acknowledged"
+    if gotUpdate:
+        f = open("offset", 'w')
+        f.write(str(last_update))
+        f.close
+        print "Stored new offset"
+    sleep(3)
